a8b8df17d0d98951bf9abfb85989f44999f03961
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-05-15 17:56:10 +1200

    Add Section09Code v2

--- src/Section09Code.java
+++ src/Section09Code.java
@@ -1,12 +1,17 @@
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Section09Code {
@@ -38,6 +43,30 @@ public class Section09Code {
 
 		waiting.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
 		System.out.println(webDriver.findElement(By.cssSelector("span.promoInfo")).getText());
+
+		// Demonstrate using a fluent wait
+		webDriver.switchTo().newWindow(WindowType.TAB);
+		webDriver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		waiting.until(ExpectedConditions.elementToBeClickable(By.id("start")));
+
+		webDriver.findElement(By.cssSelector("[id='start'] button")).click();
+
+		Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(webDriver).withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+		WebElement helloElement = waiting.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver webDriver) {
+				if (webDriver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
+					return webDriver.findElement(By.cssSelector("[id='finish'] h4"));
+				} else {
+					return null;
+				}
+			}
+		});
+
+		System.out.println("Hello World! Text is displayed: "
+				+ webDriver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
+
 	}
 
 	public static void addItems(WebDriver webDriver, String[] itemsNeeded)
