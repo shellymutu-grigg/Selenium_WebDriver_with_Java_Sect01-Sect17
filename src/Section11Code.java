946f6748e55c0d11d5836fecd26c8bbdff53eacb
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-06-12 19:08:31 +1200

    Section 11 Code updates

--- src/Section11Code.java
+++ src/Section11Code.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,6 +19,7 @@ public class Section11Code {
 		WebDriverWait waiting = new WebDriverWait(webDriver, Duration.ofSeconds(5));
 
 		// Navigate to page
+
 		webDriver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
 		// Count number of links on entire page
@@ -25,15 +27,14 @@ public class Section11Code {
 
 		System.out.println("Count of links on page: " + countAll);
 
-		// Count number links in the footer
-		// Limit driver scope to only footer
+		// Count number links in the footer // Limit driver scope to only footer
 		WebElement footerElement = webDriver.findElement(By.id("gf-BIG"));
 		int countFooter = footerElement.findElements(By.tagName("a")).size();
 
 		System.out.println("Count of links in footer: " + countFooter);
 
 		// Count number links in the footer
-		// Limit driver scope to only footer left column
+		// Limit driver scope to only footer left
 		WebElement footerColumnElement = footerElement.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
 
 		int countFooterLeftCol = footerColumnElement.findElements(By.tagName("a")).size();
@@ -47,7 +48,7 @@ public class Section11Code {
 
 			// Wait till web page has rendered
 			try {
-				Thread.sleep(5000L);
+				Thread.sleep(2000L);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -62,6 +63,67 @@ public class Section11Code {
 			System.out.println(MessageFormat.format("Tab title: {0}", currentTabNameString));
 		}
 
+		// Navigate to page
+		webDriver.get("https://www.path2usa.com/travel-companion/");
+		webDriver.manage().window().maximize();
+
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Scroll down the Page
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("window.scrollBy(0,1200)");
+
+		// Click on the Date field
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Find calendar field
+		WebElement Date = webDriver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']"));
+
+		// Click on calendar field
+		Date.click();
+
+		// Scroll through months
+		while (!webDriver.findElement(By.xpath("//div[@class='flatpickr-month']")).getText().contains("July")) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			webDriver.findElement(By.xpath("//span[@class='flatpickr-next-month']")).click();
+		}
+
+		// Count the number of days
+		int count = webDriver.findElements(By.xpath("//span[@class='flatpickr-day ']")).size();
+		for (int i = 0; i < count; i++) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			String text = webDriver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).getText();
+
+			// Only click on 26
+			if (text.equalsIgnoreCase("26")) {
+				webDriver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).click();
+				System.out.println("The selected day is: " + text);
+				break;
+			}
+		}
+
+		// Verify the date selected
+		String date = webDriver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']")).getAttribute("value");
+		System.out.println("The selected date is: " + date);
 	}
 
 }
